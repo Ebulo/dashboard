e629fb2b3c6c7960cbca549373f120eb3f35c214
fackid1 <thisfakeid.1234@gmail.com>
2021-10-10 02:23:38 +0530

    Added the initial commits

--- dashboard/landing_page/views.py
+++ dashboard/landing_page/views.py
@@ -3,5 +3,5 @@ from accounts.models import Merchant_Details
 
 
 def landing_page(request):
-    mrd = Merchant_Details.objects.all()
-    return render(request, "landing_page.html", {'merchants': mrd})
+    # mrd = Merchant_Details.objects.all()
+    return render(request, "landing_page.html")
